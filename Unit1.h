1bb4bd65ff95b092d3d983d27d2dc820b2d720d5
Marcin Zapala <marcin.zapala.programista@gmail.com>
2019-05-01 15:12:15 +0200

    first commit

---
+++ Unit1.h
@@ -0,0 +1,29 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TShape *background;
+        TImage *palletBlue;
+        TImage *palletRed;
+        TImage *ball;
+        TTimer *timerBall;
+        void __fastcall timerBallTimer(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
---
+++ Unit1.~cpp
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+int ballSpeedX = -10;
+int ballSpeedY = -10;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+        : TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::timerBallTimer(TObject *Sender)
+{
+        ball->Left += ballSpeedX;
+        ball->Top += ballSpeedY;
+}
+//---------------------------------------------------------------------------
